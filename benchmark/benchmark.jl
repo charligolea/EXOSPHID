594a9854427812b10266f74a8785455223a150c1
Carlos <charligolea@gmail.com>
2025-10-09 13:04:41 +0200

    Update benchmark

--- benchmark/benchmark.jl
+++ benchmark/benchmark.jl
@@ -1,34 +1,35 @@
+module benchmark
+
 using BenchmarkTools
 using Distributions
 
 include("../src/ExosPhID.jl")
 using .ExosPhID
 
-reaction_occurence, reaction_name, product_types, product_velocities = true, "", [], []
+export photobenchmark
 
-dt = 1.0f10 # s, Large dt to make sure interaction happens
-solar_activity = 0.0f0
-velocities = Dict("H2O" => 590.0f0, "OH" => 605.0f0, "H2" => 1750.0f0, "H" => 2500.0f0, "H(-)"=> 2500.0f0, "HO2" => 425.0f0, "H2O2" => 435.0f0, "He" => 1250.0f0, "Ne" => 560.0f0)
+function photobenchmark(dt::Float32, solar_activity::Float32, parent_name::String)
 
-for parent_name in ["H2O"] # Modify at will
+    @assert 0.0 <= solar_activity <= 1.0 "Solar activity must be in (0,1)!"
+    @assert 0.0 <= dt "dt must be positive!"
+    @assert parent_name in ("H2O", "OH", "H2", "H", "H(-)", "HO2", "H2O2", "He", "Ne") "Invalid parent species: $parent_name"
+
+    reaction_occurence, reaction_name, product_types, product_velocities = true, "", [], []
+    velocities = Dict("H2O" => 590, "OH" => 605, "H2" => 1750, "H" => 2500, "H(-)"=> 2500, "HO2" => 425, "H2O2" => 435, "He" => 1250, "Ne" => 560)
 
     print("\nStarting Numerical Benchmark for $(parent_name)\n")
 
-    parent_vector = fill(parent_name, 1)
+    parent_velocity =  velocities[parent_name]
 
-    for (index, parent_type) in enumerate(parent_vector)
-
-        if parent_type in ("H2O", "H2", "OH", "H", "H(-)", "HO2", "H2O2", "He", "Ne")
-            θ_parent, φ_parent = 2 * π * rand(), π * rand()
-            parent_velocity =  velocities[parent_type] .*  (Float32(sin(φ_parent)) * Float32(cos(θ_parent)), Float32(sin(φ_parent)) * Float32(sin(θ_parent)), Float32(cos(φ_parent)))
-        else
-            print("Parent species is not valid")
-        end
-
-        bm = @benchmark photodestruction($solar_activity, $dt, $parent_type, $parent_velocity, nothing)
-        display(bm)
-    end
+    bm = @benchmark photodestruction($solar_activity, $dt, $parent_name, $parent_velocity, nothing)
+    display(bm)
 
     print("\nNumerical Benchmark completed for $(parent_name)\n")
+
+end
+
+function photobenchmark(dt::Real, solar_activity::Real, parent_name::String)
+    return photobenchmark(Float32(dt), Float32(solar_activity), parent_name)
+end
 
 end
