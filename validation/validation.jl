d74728ba8997620d0bf75c9607aecbdac6fafa19
Carlos <charligolea@gmail.com>
2025-10-27 16:59:49 +0100

    Rename sun_mode variable in validation.jl
    change by solar_activity for consistency

--- validation/validation.jl
+++ validation/validation.jl
@@ -1,18 +1,18 @@
 """ 
-#:: FUNCTION: validate_exothermic_velocities(parent_type, sun_mode, num_reactions)
+#:: FUNCTION: validate_exothermic_velocities(parent_type, solar_activity, num_reactions)
 
 This script simulates every possible reaction for a given species a given number of times (we assume that a reaction is always happening, so infinite dt!).
 The idea is to run it many times (thousands to millions) to compare to reported values of exothermic velocities in literature.
 
 # Arguments
-- sun_mode = From 0 (Quiet Sun) to 1 (Active Sun)
+- solar_activity = From 0 (Quiet Sun) to 1 (Active Sun)
 - parent_type:: String with parent molecule type in exosphid_species
 - num_reactions:: Number of photon interactions to simulate for every possible reaction    
 """
 
 const velocities = Dict("H2O" => 590.0, "OH" => 605.0, "H2" => 1750.0, "H" => 2500.0, "H(-)"=> 2500.0, "HO2" => 425.0, "H2O2" => 435.0, "He" => 1250.0, "Ne" => 560.0)
 
-function validate_exothermic_velocities(parent_type::String, sun_mode::Float32, num_reactions::Int)
+function validate_exothermic_velocities(parent_type::String, solar_activity::Float32, num_reactions::Int)
 
     parent_velocity =  velocities[parent_type]
 
@@ -26,7 +26,7 @@ function validate_exothermic_velocities(parent_type::String, sun_mode::Float32, 
 
         print("Wavelength Range: ", wvl_range, "\n\n")
 
-        wvl_vector, energy_vector = flux_outputs(parent_type, wvl_range, sun_mode, num_reactions)
+        wvl_vector, energy_vector = flux_outputs(parent_type, wvl_range, solar_activity, num_reactions)
 
         for (r_index, rn) in enumerate(rns)
             if rps[r_index][w_index] > 0.0
@@ -41,6 +41,6 @@ function validate_exothermic_velocities(parent_type::String, sun_mode::Float32, 
 
 end
 
-function validate_exothermic_velocities(parent_type::String, sun_mode::Real, num_reactions::Int)
-    return validate_exothermic_velocities(parent_type, Float32(sun_mode), num_reactions)
+function validate_exothermic_velocities(parent_type::String, solar_activity::Real, num_reactions::Int)
+    return validate_exothermic_velocities(parent_type, Float32(solar_activity), num_reactions)
 end
