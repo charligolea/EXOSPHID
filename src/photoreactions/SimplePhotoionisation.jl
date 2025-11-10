5d2fd2a42db73f201f915e7dca161264e2ebfc96
Carlos <charligolea@gmail.com>
2025-11-10 19:11:24 +0100

    Pull Request Review
    SUMMARY OF CHANGES:
    - Replace E_photon by photon_energy to standardize scripts
    - Fix docstrings
    - Adhere to 92 character line limit
    - Minor fixes

--- src/photoreactions/SimplePhotoionisation.jl
+++ src/photoreactions/SimplePhotoionisation.jl
@@ -1,18 +1,25 @@
+# ─────────────────────────────────────────────────────────────────────────────────────────
+# FUNCTIONS
+# ─────────────────────────────────────────────────────────────────────────────────────────
+
 """ 
-    calculate_excess_energy_ionisation(E_bond, E_photon)
+    calculate_excess_energy_ionisation(E_bond, photon_energy)
 -------------------------------------------------------------------------------------------
 # Arguments
 - `E_bond::Float32` -> Ionisation energy in J
-- `E_photon::Float32` -> In in J
+- `photon_energy::Float32` -> In in J
 
 # OBJECTIVE: 
 - Calculate excess energy with a simplified approach
-- For ionization, we neglect vibrorotational contributions compared to the energy of the photon and electron
+- For ionization, we neglect vibrorotational contributions compared to the energy of the 
+    photon and electron
 
 # Output:
 - Excess energy in J
 """
-calculate_excess_energy_ionisation(E_bond::Float32, E_photon::Float32) = E_photon - E_bond
+function calculate_excess_energy_ionisation(E_bond::Float32, photon_energy::Float32)
+    return photon_energy - E_bond
+end
 
 
 
@@ -26,13 +33,15 @@ calculate_excess_energy_ionisation(E_bond::Float32, E_photon::Float32) = E_photo
 - `p_photon::NTuple{3, Real}` -> 3D Tuple in kg*m/s
 
 # OBJECTIVE: 
-- Calculate velocities for the photoionisation products according to the moment and energy conservation equations
+- Calculate velocities for the photoionisation products according to the moment and energy 
+    conservation equations
 - The derivations of the quadratic equation can be consulted in the EXOSPHID WIKI
 
 # Output:
 - `v_ion_tuple`: 3D Tuple containing velocity components for the ionised parent
 """
-function allocate_velocity_ionisation(reaction::PhotoReaction, E_excess::Real, species_masses::Float64, p_photon::NTuple{3, Real})
+function allocate_velocity_ionisation(reaction::PhotoReaction, E_excess::Real, 
+                                    species_masses::Float64, p_photon::NTuple{3, Real})
 
     # 1. Get unitary vector for photon
     u_ph = reaction.sun_tuple
@@ -48,84 +57,94 @@ function allocate_velocity_ionisation(reaction::PhotoReaction, E_excess::Real, s
     u_ion = random_unit_tuple()
 
     # 5. Calculate speed values for electron and ion
-    a = m_el * (m_ion * (dot(u_ion,u_ph)))^2 + (m_el^2 * m_ion)*(dot(u_el, u_ph)^2)
+    a = m_el * (m_ion * (dot(u_ion,u_ph)))^2 + 
+        (m_el^2 * m_ion)*(dot(u_el, u_ph)^2)
     b = -2 * (dot(u_el, u_ph) * m_el * m_ion) * (dot(total_momentum,u_ph))
-    c = m_ion * dot(total_momentum,u_ph)^2 - 2 * E_excess * (m_ion * (dot(u_ion,u_ph)))^2
+    c = m_ion * dot(total_momentum,u_ph)^2 - 
+        2 * E_excess * (m_ion * (dot(u_ion,u_ph)))^2
 
     Δ = b^2 - 4 * a * c
     v_el_mag = Δ ≥ 0 ? (-b + sqrt(Δ)) / (2 * a) : 0
     v_el_tuple= v_el_mag .* u_el
 
-    v_ion_tuple= (total_momentum .- m_el.*v_el_tuple) ./ m_ion
+    v_ion_tuple= (total_momentum .- m_el.* v_el_tuple) ./ m_ion
 
     return Float32.(v_ion_tuple)
 end
 
 
 """
-    simulate_photoionisation(reaction, E_photon)
+    simulate_photoionisation(reaction, photon_energy)
 -------------------------------------------------------------------------------------------
 # Arguments
 - `reaction::PhotoReaction`
-- `E_photon::Float32` -> In in J
+- `photon_energy::Float32` -> In in J
 
 # OBJECTIVE: 
-- Simulate a single photoionisation reaction that has previously been determined from the database
+- Simulate a single photoionisation reaction that has previously been determined from the 
+    database
 
 # Output: Output from `allocate_velocity()`
 - `v_ion_tuple`: 3D Tuple containing velocity components for the ionised parent
 """
-function simulate_photoionisation(reaction::PhotoReaction, E_photon::Float32)
+function simulate_photoionisation(reaction::PhotoReaction, photon_energy::Float32)
 
     # 1. Calculate photon linear momentum magnitude (kg*m/s)
-    p_photon = calculate_photon_momentum(E_photon, reaction.sun_tuple)
+    p_photon = calculate_photon_momentum(photon_energy, reaction.sun_tuple)
 
     # 2. Get masses for the parent species (also the mass of the product ion!)
     species_masses = get_masses(reaction.product_types[1]; mode="PI")
 
     # 3. Calculate excess energy for reaction (J)
-    E_excess = calculate_excess_energy_ionisation(reaction.E_bond, E_photon)
+    E_excess = calculate_excess_energy_ionisation(reaction.E_bond, photon_energy)
 
     # 4. Calculate velocity of product ion
-    v_ion_tuple = allocate_velocity_ionisation(reaction, E_excess, species_masses, p_photon)
+    v_ion_tuple = 
+        allocate_velocity_ionisation(reaction, E_excess, species_masses, p_photon)
 
     return v_ion_tuple
 end
 
-function simulate_photoionisation(reaction::PhotoReaction, E_photon::Real)
-    return simulate_photoionisation(reaction, Float32(E_photon))
+function simulate_photoionisation(reaction::PhotoReaction, photon_energy::Real)
+    return simulate_photoionisation(reaction, Float32(photon_energy))
 end
 
 
 """
-    multiple_photoionisation(reaction, energy_vector)
+    multiple_photoionisation(reaction, photon_energy_vector)
 -------------------------------------------------------------------------------------------
 # Arguments
 - `reaction::PhotoReaction`
-- `energy_vector::Vector{Float32}` -> contains N scalar values of photon energy in J in the wavelength range of choice
+- `photon_energy_vector::Vector{Float32}` -> contains N scalar values of photon energy in J 
+    in the wavelength range of choice
 
 # OBJECTIVE: 
 - Function to simulate multiple ionisation reactions
-- Particularly interesting for validation studies ehere we want to generate multiple photons at the same time for a specific parent 
+- Particularly interesting for validation studies ehere we want to generate multiple photons 
+    at the same time for a specific parent 
 AND wavelength range  AND reaction type and compare to literature values
 
 # Output: Outputs from `allocate_velocity()`
-- `final_speeds_ion`: Array of Size N. Every element is a 3D Tuple containing velocity components for the ionised parent
+- `final_speeds_ion`: Array of Size N. Every element is a 3D Tuple containing velocity 
+    components for the ionised parent
 """
-function multiple_photoionisation(reaction::PhotoReaction, energy_vector::Vector{Float32})
+function multiple_photoionisation(reaction::PhotoReaction, 
+                                photon_energy_vector::Vector{Float32})
 
     if reaction.display_info
-        println("Simulating photoionisation reaction: " *""* reaction.product_names[1] *""* " + γ - > " *""* reaction.product_names[2] *" + e-")
+        println("Simulating photoionisation reaction: " 
+            *""* reaction.product_names[1] *""* " + γ - > " 
+            *""* reaction.product_names[2] *" + e-")
     end
 
     final_speeds_ion = []
 
     # 1. Loop over photon energy vector
-    for E_photon in energy_vector
+    for photon_energy in photon_energy_vector
 
-        if E_photon > reaction.E_bond
+        if photon_energy > reaction.E_bond
             # 1.1. Simulate individual photoreaction for every incoming photon
-            v_ion_tuple= simulate_photoionisation(reaction, E_photon)
+            v_ion_tuple= simulate_photoionisation(reaction, photon_energy)
 
             # 1.2. Store trajectories and speeds of every ion product
             push!(final_speeds_ion, v_ion_tuple./1000)
@@ -141,8 +160,9 @@ function multiple_photoionisation(reaction::PhotoReaction, energy_vector::Vector
     return final_speeds_ion
 end
 
-function multiple_photoionisation(reaction::PhotoReaction, energy_vector::AbstractVector{<:Real})
-    return multiple_photoionisation(reaction, Float32.(collect(energy_vector)))
+function multiple_photoionisation(reaction::PhotoReaction, 
+                                photon_energy_vector::AbstractVector{<:Real})
+    return multiple_photoionisation(reaction, Float32.(collect(photon_energy_vector)))
 end
 
 
@@ -158,7 +178,7 @@ end
 - Only if `display_info` is set true
 """
 function show_info_ionisation(reaction::PhotoReaction, final_speeds_ion::Vector{Any})
-    final_speeds_ion_norm = [norm(p) for p in final_speeds_ion]
+    final_speeds_ion_norm = norm.(final_speeds_ion)
 
     data_speeds = DataFrame(
     Product = [reaction.product_names[1]],
@@ -169,6 +189,10 @@ function show_info_ionisation(reaction::PhotoReaction, final_speeds_ion::Vector{
     println("")
 end
 
+
+# ─────────────────────────────────────────────────────────────────────────────────────────
+# EXPORTS
+# ─────────────────────────────────────────────────────────────────────────────────────────
 
 export simulate_photoionisation
 export multiple_photoionisation
