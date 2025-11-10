b6e91e665bbf22295fe00ee3d30ab7d8fe95c611
Carlos <charligolea@gmail.com>
2025-11-10 20:33:28 +0100

    Adhere to 92 character line limit

--- src/database/species/general_construct.jl
+++ src/database/species/general_construct.jl
@@ -1,28 +1,30 @@
-"""
-─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+# ─────────────────────────────────────────────────────────────────────────────────────────
 # GENERAL SPECIES OBJECT
-─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
-"""
+# ─────────────────────────────────────────────────────────────────────────────────────────
 
 """
-    Species:
+    Species
 
 A general data structure for defining the photochemical database of a given species.
-Each Species instance collects all relevant parameters for modeling photodestruction and photoionisation channels, as well as their probabilities across wavelength intervals.
+Each Species instance collects all relevant parameters for modeling photodestruction and 
+    photoionisation channels, as well as their probabilities across wavelength intervals.
 
 # INPUTS
 
-- `wvl_threshold::Float32` -> Threshold wavelength (in Å) above which photodestruction is negligible.
+- `wvl_threshold::Float32` -> Threshold wavelength (in Å) above which photodestruction is 
+    negligible.
 
 - `quiet_rate::Float32` -> Photodestruction rate under quiet Sun conditions (s⁻¹).
 - `active_rate::Float32` -> Photodestruction rate under active Sun conditions (s⁻¹).
 
-- `tsh_energies::Tuple` -> Threshold energies (in eV) for each photoprocess channel. The structure varies depending on the reaction type:
+- `tsh_energies::Tuple` -> Threshold energies (in eV) for each photoprocess channel. 
+    The structure varies depending on the reaction type:
     * SPD: single Float32 (dissociation energy).
     * SPI: single Float32 (ionisation threshold).
     * DPD, DPI, DiPI: 2D tuples of Float32 (first-step and full-process energies).
 
-- `species_names::Tuple` -> Parent and product species for each channel. The structure varies depending on the reaction type:
+- `species_names::Tuple` -> Parent and product species for each channel. The structure 
+   varies  depending on the reaction type:
     * SPD/SPI: 3-element tuple (parent, product1, product2).
     * DPD/DPI/DiPI: nested tuples of 3-element tuples describing multi-step processes.
 Electronic states may and should be included, e.g. "OH(X²Π)".
@@ -30,14 +32,16 @@ Electronic states may and should be included, e.g. "OH(X²Π)".
 - `reaction_probabilities::Tuple` -> Branching ratios (Float32) per wavelength range.
 For a given wavelength, probabilities across all channels must sum to 1.
 
-- `reaction_names::Tuple` -> Identifiers for each reaction channel (custom labels for later analysis).
+- `reaction_names::Tuple` -> Identifiers for each reaction channel (custom labels for 
+    later analysis).
 
 - `reaction_types::Tuple` -> Interaction class, one of:
     * "SPD", "DPD", "SPI", "DPI", "DiPI"
 corresponding respectively to single/double photodissociation and ionisation processes.
 
-- `wavelength_range::NTuple{2, Float32}`` -> Photon wavelength intervals (in Å) for which each reaction is active.
-Each entry is a 2-tuple of Float32 defining the lower and upper wavelength bounds.
+- `wavelength_range::NTuple{2, Float32}`` -> Photon wavelength intervals (in Å) for 
+    which each reaction is active. Each entry is a 2-tuple of Float32 defining the 
+    lower and upper wavelength bounds.
 
 # EXAMPLE:
 H2O = Species(
@@ -53,7 +57,8 @@ H2O = Species(
 )
 
 # TESTING:
-- Tests are available to make sure that the photochemical database is correctly built. See run_tests.jl
+- Tests are available to make sure that the photochemical database is correctly built. 
+- See `run_tests.jl`
 """
 struct Species
     wvl_threshold::Float32
